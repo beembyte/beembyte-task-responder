a06fc45022dbffdf5639a8e88a231d368cf05f60
Watchdog <53454418+watchDOGGGG@users.noreply.github.com>
2025-06-07 07:56:24 +0100

    update

--- src/components/task/TaskSidebar.tsx
+++ src/components/task/TaskSidebar.tsx
@@ -139,8 +139,10 @@ const TaskSidebar: React.FC<TaskSidebarProps> = ({
 
           {/* Deadline Progress Bar */}
           <DeadlineProgressBar task={task} />
+          {isTaskAccepted &&
+            <Separator />
+          }
 
-          <Separator />
           <div className="flex items-center gap-3">
             <Clock className="w-5 h-5 text-gray-400" />
             <div>
